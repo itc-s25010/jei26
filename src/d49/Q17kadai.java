a7adde9d7db73fc43f744cd258640c855cdb9e94
itc-s25010 <s25010@std.it-college.ac.jp>
2026-05-22 12:15:36 +0900

    課題

---
+++ src/d49/Q17kadai.java
@@ -0,0 +1,10 @@
+package d49;
+
+public class Q17kadai {
+    static void main(String[] args){
+        int n = 1;
+
+        System.out.println(++n + " " + --n);
+       // System.out.print(n);
+    }
+}
