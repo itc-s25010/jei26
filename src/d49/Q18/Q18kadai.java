65943b1274ad1e8d44f2e8f6496e270d7ea1d154
itc-s25010 <s25010@std.it-college.ac.jp>
2026-05-22 12:23:06 +0900

    課題

---
+++ src/d49/Q18/Q18kadai.java
@@ -0,0 +1,11 @@
+package d49Q18;
+
+public class Q18kadai {
+    static void main(String[] args){
+        int total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println(total / capacity * (double)cargo);
+    }
+}
