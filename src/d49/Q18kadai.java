a4d95caf8889d5d565c0acd10585981c035a4fa6
itc-s25010 <s25010@std.it-college.ac.jp>
2026-05-22 12:28:41 +0900

    課題

--- src/d49/Q18kadai.java
+++ src/d49/Q18kadai.java
@@ -9,3 +9,9 @@ public class Q18kadai {
         System.out.println(total / capacity * (double)cargo);
     }
 }
+
+/*
+total、capacityのどちらかをdouble型にすると小数点以下が切り捨てられないため2.5 * 2 = 5.0になるが
+cargoにつければtotal/capacityの小数点以下は切り捨てられ
+2 * 2.0 = 4.0になるため
+ */
